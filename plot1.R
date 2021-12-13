43637567ad07aca3c558270f8d58eb8e912af30d
jmang6 <85960803+jmang6@users.noreply.github.com>
2021-12-13 15:50:59 -0500

    Initial upload

---
+++ plot1.R
@@ -0,0 +1,24 @@
+# Code to download and save .zip files for project
+
+zipURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(zipURL, destfile = "~/GitHub/CourseraPersonalPC/Practical Assignment/Exploratory Data Analysis - Week 1 Practical Assignment/zipfile.zip")
+
+# Code to unzip the .zip files for project
+
+projectfiles <- "~/GitHub/CourseraPersonalPC/Practical Assignment/Exploratory Data Analysis - Week 1 Practical Assignment/zipfile.zip"
+unzip(projectfiles)
+
+# Code to read in all data and combine Date and Time variables
+
+projectdata <- read.table("~/GitHub/CourseraPersonalPC/Practical Assignment/Exploratory Data Analysis - Week 1 Practical Assignment/household_power_consumption.txt", header = TRUE, sep = ";", na.string = "?")
+projectdata$Date <- as.Date(projectdata$Date, format = "%d/%m/%Y")
+
+dataforplot <- subset(projectdata, Date >= "2007-02-01" & Date <= "2007-02-02")
+
+dataforplot$DateTime <- as.POSIXct(as.character(paste(dataforplot$Date, dataforplot$Time)), format = "%Y-%m-%d %H:%M:%S")
+
+# Histogram for specific date range and global active power
+
+png("plot1.png", width = 480, height = 480)
+hist(dataforplot$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
+dev.off()
